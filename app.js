61609551c02135ee77ae4f09ca712bacdf534236
Gaurav patel <164714270+Gaurav0135@users.noreply.github.com>
2024-09-12 11:59:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+    // console.log(Process.env.SECRET); 
+    }
+    
+    const express = require("express");
+    const app = express();
+    const mongoose = require("mongoose");
+    const path = require("path");
+    const methodOverride = require("method-override");
+    const ejsMate = require("ejs-mate");
+    const ExpressError = require("./utils/ExpressError.js");
+    const session = require("express-session");
+    const MongoStore = require("connect-mongo");
+    const flash = require("connect-flash");
+    const passport = require("passport");
+    const LocalStrategy = require("passport-local");
+    const User = require("./models/user.js");
+    
+     
+    const listingRouter = require("./routes/listing.js");
+    const reviewRouter =  require("./routes/review.js");
+    const userRouter = require("./routes/user.js");
+    
+    
+    const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+    
+    main()
+      .then(() => {
+        console.log("connected to DB");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
+    async function main() {
+      await mongoose.connect(MONGO_URL);  
+    }
+    
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "views"));
+    
+    
+    app.use(express.urlencoded({ extended: true }));
+    app.use(methodOverride("_method"));
+    app.engine("ejs", ejsMate);
+    app.use(express.static(path.join(__dirname , "/public")));
+    
+    
+    
+    const sessionOptions = {
+
+      secret: "my supersecretcode",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        expires: Date.now()  + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      },
+    };
+    
+
+    app.use(session(sessionOptions));
+    app.use(flash());
+    
+    
+     app.use(passport.initialize());
+     app.use(passport.session());
+     passport.use(new LocalStrategy(User.authenticate()));
+    
+     passport.serializeUser(User.serializeUser());
+     passport.deserializeUser(User.deserializeUser());
+    
+    
+    app.use((req, res, next) => {
+      res.locals.Success = req.flash("Success");
+      res.locals.error = req.flash("error");
+      res.locals.currUser = req.user;
+      next();
+    });
+    
+   
+    app.use("/listings", listingRouter);
+    app.use("/listings/:id/reviews" , reviewRouter);
+    app.use("/" , userRouter);
+    
+    
+    app.all("*", ( req, res, next ) => {
+      next(new ExpressError(404 , "Page Not Found!!"));
+    } );
+    
+    app.use((err, req, res, next) => {
+      let {statusCode = 500 , message="Something Went Wrong"} = err;
+      res.status(statusCode).render("error.ejs" , { message });
+      
+    });
+    
+    app.listen(8080, () => {
+      console.log("server is listening to port 8080");
+    });
